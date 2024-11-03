e16ec8c76de20211df6b7ee8919cfe6aaacaa69a
misung.dev <misung.dev@gmail.com>
2024-11-03 21:05:43 +0900

    #7 [FEAT] 기본 스타일 적용

--- src/pages/ProjectPage/ProjectPage.style.jsx
+++ src/pages/ProjectPage/ProjectPage.style.jsx
@@ -3,5 +3,9 @@ import styled from 'styled-components';
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
-  border: 1px solid blue;
+  justify-content: center;
+  max-width: 1100px;
+  width: 100%;
+  margin: 0 auto;
+  border: 1px solid blue; /* 임시코드 */
 `;
