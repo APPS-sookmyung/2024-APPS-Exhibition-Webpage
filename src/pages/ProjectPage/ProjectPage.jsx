c8dded2dcefebc4225fabeac34d78089b6042474
Kim Junhee <100200965+junheekim61@users.noreply.github.com>
2024-11-04 01:34:10 +0900

    #10 [FEAT] 프로젝트 페이지 배경 설정 및 상단 부분 구현

--- src/pages/ProjectPage/ProjectPage.jsx
+++ src/pages/ProjectPage/ProjectPage.jsx
@@ -1,5 +1,27 @@
-import * as S from './ProjectPage.style';
+import * as s from './ProjectPage.style';
 
 export default function ProjectPage() {
-  return <S.Container>프로젝트 상세 페이지</S.Container>;
+  return (
+    <s.Root>
+      <s.Container>
+        <s.CloseBtn />
+        <s.Top>
+          <s.Title>COOKIEE-</s.Title>
+          <s.Description>하루하루의 쿠키를 모아보세요!</s.Description>
+          <s.TopBtnContainer>
+            <s.ShareBtn>
+              <s.ShareIcon />
+              <p>프로젝트 공유하기</p>
+            </s.ShareBtn>
+            <s.LinkBtns>
+              <s.LinkBtn />
+              <s.LinkBtn />
+              <s.LinkBtn />
+              <s.LinkBtn />
+            </s.LinkBtns>
+          </s.TopBtnContainer>
+        </s.Top>
+      </s.Container>
+    </s.Root>
+  );
 }
--- src/pages/ProjectPage/ProjectPage.style.jsx
+++ src/pages/ProjectPage/ProjectPage.style.jsx
@@ -1,11 +1,101 @@
 import styled from 'styled-components';
+import x_pink from '../../images/icons/x_pink.svg';
+import background from '../../images/background/projects_background.svg';
+import share from '../../images/icons/share.svg';
+
+export const Root = styled.div`
+  width: 100%;
+  min-height: 5000px;
+  margin: 0 auto;
+  background-image: url(${background});
+  background-size: contain;
+  background-repeat: no-repeat;
+  background-color: #000;
+`;
 
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
-  max-width: 1100px;
+  /* max-width: 1300px; */
+  padding: 62px 70px 0 70px;
+`;
+
+export const CloseBtn = styled.img.attrs({
+  src: x_pink,
+  alt: 'Close',
+})`
+  align-self: flex-end;
+  width: 36px;
+  height: 36px;
+`;
+
+export const Top = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: baseline;
+  justify-content: center;
+  color: #fff;
+`;
+
+export const Title = styled.h1`
+  font-size: 65px;
+  margin: 0;
+  margin-bottom: 4px;
+`;
+
+export const Description = styled.p`
+  font-size: 32px;
+  font-weight: 500;
+  letter-spacing: -1.6px;
+  margin: 0 0 22px 0;
+`;
+
+export const TopBtnContainer = styled.div`
+  display: flex;
   width: 100%;
-  margin: 0 auto;
-  border: 1px solid blue; /* 임시코드 */
+  justify-content: space-between;
+`;
+
+export const ShareBtn = styled.div`
+  background-color: #ff88fb;
+  border-radius: 32px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  padding: 13px 43px;
+  margin: 0;
+
+  p {
+    color: #fff;
+    text-align: center;
+    font-size: 20px;
+    font-weight: 600;
+    letter-spacing: -1px;
+    max-height: 24px;
+    margin: 0;
+  }
+`;
+
+export const ShareIcon = styled.img.attrs({
+  src: share,
+  alt: 'Share',
+})`
+  width: 20px;
+  height: 24px;
+  margin-right: 10px;
+`;
+
+export const LinkBtns = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 12px;
+`;
+
+export const LinkBtn = styled.div`
+  width: 50px;
+  height: 50px;
+  border-radius: 10px;
+  background-color: #fff;
 `;
