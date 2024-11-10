1e32ef3d00a75f4a77f6da148c8012c9599883e4
misung.dev <misung.dev@gmail.com>
2024-11-11 00:09:09 +0900

    #26 [FEAT] 홈화면 기본 구성

--- src/pages/HomePage/HomePage.style.jsx
+++ src/pages/HomePage/HomePage.style.jsx
@@ -1,11 +1,59 @@
 import styled from 'styled-components';
 
-export const Container = styled.div`
+export const HomePage = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
   max-width: 1100px;
   width: 100%;
   margin: 0 auto;
-  border: 1px solid blue; /* 임시코드 */
+  border: 1px solid red; /* 임시코드 */
 `;
+
+export const HomeMainWrapper = styled.section`
+  background-image: url('../../images/background/home-background1.svg');
+`;
+
+export const TitleWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: end;
+  padding: 70px;
+`;
+
+export const ExhibitionTitle = styled.h3`
+  color: #fff;
+  text-align: center;
+  font-family: 'Dolce Vita Heavy';
+  font-size: 65px;
+  font-weight: 700;
+  letter-spacing: -3.25px;
+`;
+
+export const APPSTitle = styled.h1`
+  color: #fff;
+  text-align: center;
+  font-family: 'Dolce Vita Heavy';
+  font-size: 100px;
+  font-weight: 700;
+  letter-spacing: -5px;
+`;
+
+export const APPSSubTitle = styled.span`
+  color: #c7c7c7;
+  text-align: center;
+  font-family: 'Dolce Vita Heavy';
+  font-size: 24px;
+  font-style: normal;
+  font-weight: 700;
+  line-height: normal;
+  letter-spacing: -1.2px;
+`;
+
+export const AboutAPPS = styled.section``;
+
+export const MemberFeedback = styled.section``;
+
+export const SocialLinks = styled.section``;
+
+export const ProjectList = styled.section``;
