ba67c83105de4c3c664548861cd11ded38a3f09b
misung.dev <misung.dev@gmail.com>
2024-11-10 22:49:16 +0900

    #24 [DESIGN] header 배경색 변경

--- src/components/Header/Header.style.jsx
+++ src/components/Header/Header.style.jsx
@@ -9,10 +9,11 @@ export const Header = styled.header`
   display: flex;
   justify-content: space-between;
   align-items: center;
+  margin: 0 auto;
+  background: #212121;
   max-width: 1100px;
   width: 100%;
   height: 60px;
-  margin: 0 auto;
 `;
 
 export const Logo = styled.div`
