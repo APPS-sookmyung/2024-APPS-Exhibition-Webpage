562f4590265da8959abe5e8d22bf3ab315d8dc5b
j1m1n-ha <163178666+j1m1n-ha@users.noreply.github.com>
2024-11-14 14:17:10 +0900

    #30 [FEAT] Footer 기능 구현

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,5 +1,38 @@
 import * as S from './Footer.style';
 
 export default function Footer() {
-  return <S.Footer>footer</S.Footer>;
+  return (
+    <S.FooterWrapper>
+      <S.Footer>
+        <S.Logo>
+          <img src="/images/logo/logo-white-footer.svg" alt="APPS 로고" />
+        </S.Logo>
+
+        <S.DeveloperSection>
+          <S.SectionTitle>DEVELOPER</S.SectionTitle>
+          <S.TeamMemberContainer>
+            <S.TeamMember>김준희 (소프트웨어학부 22)</S.TeamMember>
+            <S.TeamMember>류미성 (소프트웨어학부 21)</S.TeamMember>
+            <S.TeamMember>이다빈 (소프트웨어학부 24)</S.TeamMember>
+            <S.TeamMember>하지민 (수학과, 소프트웨어학부 22)</S.TeamMember>
+          </S.TeamMemberContainer>
+        </S.DeveloperSection>
+
+        <S.DesignerSection>
+          <S.SectionTitle>DESIGNER</S.SectionTitle>
+          <S.TeamMemberContainer>
+            <S.TeamMember>김수진 (공예과 21)</S.TeamMember>
+            <S.TeamMember>문서영 (산업디자인과 22)</S.TeamMember>
+          </S.TeamMemberContainer>
+        </S.DesignerSection>
+
+        <S.InfoSection>
+          <S.Address>
+            숙명여자대학교 | 04310 서울특별시 용산구 청파로47길 100 (청파동2가)
+          </S.Address>
+          <S.Copyright>Copyright APPS. All Rights Reserved.</S.Copyright>
+        </S.InfoSection>
+      </S.Footer>
+    </S.FooterWrapper>
+  );
 }
