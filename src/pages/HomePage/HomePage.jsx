8353b156bdacde62757b02c7ec181dee08a825b0
misung.dev <misung.dev@gmail.com>
2024-11-17 22:51:58 +0900

    #39 [FEAT] 12기 알림 신청하기 링크 추가

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -20,8 +20,15 @@ export default function HomePage() {
         <S.ShareLinks>
           <S.ShareLinksTitle>함께 여정을 떠나볼까요?</S.ShareLinksTitle>
           <S.ShareLinksContent>
-            <S.LinkBox>링크 공유하기</S.LinkBox>
-            <S.LinkBox>12기 알림 신청하기</S.LinkBox>
+            <S.StyledLink to="/">
+              <S.LinkBox>링크 공유하기</S.LinkBox>
+            </S.StyledLink>
+            <S.StyledLink
+              to="https://forms.gle/k14eA7Kk9bxD7aTm6"
+              target="_blank"
+            >
+              <S.LinkBox>12기 알림 신청하기</S.LinkBox>
+            </S.StyledLink>
           </S.ShareLinksContent>
         </S.ShareLinks>
       </S.HomePage>
