e2eb6f8dac232937a46cc66ab47bb35dc91a9e57
misung.dev <misung.dev@gmail.com>
2024-11-17 18:37:54 +0900

    #33 [REFACTOR] APPS 핵심 가치 카드 데이터 분리

---
+++ src/database/appsCoreValueCards.js
@@ -0,0 +1,30 @@
+export const APPS_CORE_VALUE_CARDS = [
+  {
+    imageSrc: '/images/icons/icon-with-A.svg',
+    title: '탐구',
+    description:
+      '다양한 분야와 주제에 대한 탐색\n#DevTalk #스터디 #컨퍼런스참여',
+    color: '#FF88FB',
+  },
+  {
+    imageSrc: '/images/icons/icon-with-P-green.svg',
+    title: '혁신',
+    description:
+      '개발자가 되기 위한 끝없는 도전\n#신입부원 웹 프로젝트 #서브스터디',
+    color: '#5BFB67',
+  },
+  {
+    imageSrc: '/images/icons/icon-with-P-orange.svg',
+    title: '협력',
+    description:
+      '스터디와 프로젝트를 통한 개발 실력 향상\n#스터디 #프로젝트 #대외활동',
+    color: '#FF5400',
+  },
+  {
+    imageSrc: '/images/icons/icon-with-S.svg',
+    title: '성장',
+    description:
+      '기수간 꾸준한 소통 및 정보 공유\n#정기세미나 #멘토링 #친목활동',
+    color: '#3F69FF',
+  },
+];
