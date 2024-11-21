3088209389d62ed44243fcabcf82f023facb0f47
misung.dev <misung.dev@gmail.com>
2024-11-22 01:50:15 +0900

    #58 [FEAT] 프로젝트 카드 구현

--- src/components/ProjectCardList/ProjectCardList.jsx
+++ src/components/ProjectCardList/ProjectCardList.jsx
@@ -26,9 +26,21 @@ const ProjectCardList = () => {
       </S.TabBar>
 
       <S.ProjectCardContainer>
-        {PROJECT_LIST.map((project) => {
-          <S.ProjectCard></S.ProjectCard>;
-        })}
+        {/* {PROJECT_LIST.map((project) => { */}
+        <S.ProjectCard>
+          <S.ProjectCardImage>임시</S.ProjectCardImage>
+          <S.ProjectCardContent>
+            <S.ProjectTitleWrapper>
+              <S.ProjectTitle>HABOR!</S.ProjectTitle>
+              <S.ProjectSubTitle>2030 멘토 -멘티 커뮤니티</S.ProjectSubTitle>
+            </S.ProjectTitleWrapper>
+            <S.ProjectBadgeWrapper>
+              <S.ProjectBadge># 4기</S.ProjectBadge>
+              <S.ProjectBadge># 4기</S.ProjectBadge>
+            </S.ProjectBadgeWrapper>
+          </S.ProjectCardContent>
+        </S.ProjectCard>
+        ;{/* })} */}
       </S.ProjectCardContainer>
     </S.ProjectCardList>
   );
--- src/components/ProjectCardList/ProjectCardList.style.jsx
+++ src/components/ProjectCardList/ProjectCardList.style.jsx
@@ -1,6 +1,10 @@
 import styled from 'styled-components';
 
-export const ProjectCardList = styled.div``;
+export const ProjectCardList = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 60px;
+`;
 
 export const TabBar = styled.div`
   display: flex;
@@ -30,4 +34,71 @@ export const Tab = styled.div`
 
 export const ProjectCardContainer = styled.div``;
 
-export const ProjectCard = styled.div``;
+export const ProjectCard = styled.div`
+  display: flex;
+`;
+
+export const ProjectCardImage = styled.div`
+  width: 310px;
+  height: 260px;
+  flex-shrink: 0;
+  border-radius: 20px;
+
+  background-color: orange;
+`;
+
+export const ProjectCardContent = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  width: 140px;
+  height: 260px;
+  border-radius: 20px;
+  background: #f1f1f1;
+  padding: 28px 24px;
+  box-sizing: border-box;
+  transition:
+    background-color 0.3s ease,
+    transform 0.2s ease;
+
+  &:hover {
+    background-color: #ff88fb;
+  }
+`;
+
+export const ProjectTitleWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+export const ProjectTitle = styled.strong`
+  color: #818181;
+  font-family: 'Dolce Vita Heavy';
+  font-size: 24px;
+  font-weight: 700;
+`;
+
+export const ProjectSubTitle = styled.span`
+  color: #818181;
+  font-size: 16px;
+  font-weight: 500;
+`;
+
+export const ProjectBadgeWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  gap: 8px;
+`;
+
+export const ProjectBadge = styled.div`
+  left: 0;
+  border-radius: 20px;
+  background: #818181;
+  padding: 5px 16px;
+  color: #fff;
+  font-size: 14px;
+  transition:
+    background-color 0.3s ease,
+    transform 0.2s ease;
+`;
