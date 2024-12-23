88cd6623a0f41da2e173acc365ea6a792653ed78
ninum12 <ninum@sookmyung.ac.kr>
2024-12-23 23:41:27 +0900

    #106 [FEAT] 폰트 파일 추가 및 적용

--- src/styles/index.js
+++ src/styles/index.js
@@ -1,4 +1,5 @@
 export * from './exhibitionStyles.style';
 export * from './resetStyles.style';
 export * from './PretendardFont.style';
+export * from './DolceVitaFont.style';
 export * from './mediaQueries.style';
