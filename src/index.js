416cba84b52aa9fed01a164070e87e548fb3ae87
misung.dev <misung.dev@gmail.com>
2024-12-23 20:37:33 +0900

    #89 [FEAT] 폰트 파일 추가

--- src/index.js
+++ src/index.js
@@ -3,11 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { exhibitionStyles, pretendardFont, resetStyles } from './styles';
+import { css } from 'styled-components';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App
+      styles={css`
+        ${resetStyles}
+        ${exhibitionStyles}
+        ${pretendardFont}
+      `}
+    />
   </React.StrictMode>,
 );
 
