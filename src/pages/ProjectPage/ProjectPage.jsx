3e03a293923da29f5ab8a023403ecf3fac03e99c
ninum12 <ninum@sookmyung.ac.kr>
2024-12-25 23:18:25 +0900

    #156 [FIX] 오류 수정 시도 1

--- src/pages/ProjectPage/ProjectPage.jsx
+++ src/pages/ProjectPage/ProjectPage.jsx
@@ -115,7 +115,6 @@ export default function ProjectPage() {
                 height="315"
                 src={`https://www.youtube.com/embed/${projectData.youtubeVideoId}`}
                 title="YouTube video player"
-                frameBorder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; 
               gyroscope; picture-in-picture"
                 allowFullScreen
